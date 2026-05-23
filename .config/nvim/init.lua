3d8943080ea34dfdeb7e4c3c0594d457b68381d1
KenAragon1 <dev.kennyaragonshn@gmail.com>
2026-05-23 22:42:39 +0700

    update: nvim minimal config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,58 +1,60 @@
-require("config.lazy")
-require("keymaps")
+vim.opt.termguicolors = true    -- enable 24-bit colors
 
-vim.schedule(function()
-    vim.o.clipboard = "unnamedplus"
-end)
-
-vim.opt.laststatus = 3
-
+-- basic settings
+vim.opt.number = true           -- line number
+vim.opt.relativenumber = true   -- relative line number
+vim.opt.cursorline = false       -- Highlight current line
+vim.opt.wrap = false            -- Wrap lines
 vim.opt.linebreak = true
+vim.opt.sidescrolloff = 10      -- Keep n lines left/right cursor
+vim.opt.scrolloff = 8           -- Keep n lines above/below cursor
+vim.opt.swapfile = false
+vim.opt.showtabline = 1
 
-vim.opt.termguicolors = true
+-- find files
+vim.opt.path:append("**")
+vim.opt.wildmenu = true
 
-vim.opt.shiftwidth = 4
+-- indentation
 vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
 vim.opt.expandtab = true
-
 vim.opt.smartindent = true
-
-vim.opt.number = true
-vim.opt.relativenumber = true
-
-vim.opt.termguicolors = true
-
-vim.opt.mouse = 'a'
-
-vim.opt.showmode = false
-
-vim.opt.scrolloff = 8
-
-vim.opt.updatetime = 250
-
-vim.opt.pumheight = 15
-vim.opt.cursorline = true
-
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-
-vim.opt.wrap = true
 vim.opt.breakindent = true
 
+-- search settings
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.incsearch = true
+
+-- visual settings 
+vim.opt.signcolumn = 'yes'
+vim.opt.colorcolumn = ""
+vim.opt.showmode = true
+vim.opt.laststatus = 2
+vim.opt.pumheight = 15
+vim.o.list = true
+vim.opt.listchars = "tab:» ,trail:·,extends:>,precedes:<,nbsp:+,lead:·"
+
+-- backup
 vim.opt.undofile = true
 
-vim.opt.signcolumn = 'yes'
+-- behaviour settings
+vim.opt.mouse = 'a'
+vim.o.clipboard = "unnamedplus"
 
+-- split behaviour
 vim.o.splitright = true
 vim.o.splitbelow = true
 
-vim.opt.list = false
-vim.opt.listchars = {
-    tab = '>> ',
-    trail = '·',
-    nbsp = '␣'
-}
+vim.opt.updatetime = 250
+
+-- floating window
+vim.opt.winborder = "single"
+vim.api.nvim_set_hl(0, 'NormalFloat', {
+    bg = 'NONE'
+})
 
 vim.api.nvim_create_autocmd('TextYankPost', {
     desc = 'Highlight when yanking text',
@@ -61,3 +63,109 @@ vim.api.nvim_create_autocmd('TextYankPost', {
         vim.hl.on_yank()
     end,
 })
+
+-- =================================================
+-- Keymaps
+-- =================================================
+
+vim.g.mapleader = " "
+
+vim.keymap.set("n", '<leader>e', ':Ex<CR>', { desc = "Open Netrw" })
+
+-- center screen when jumping
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<C-f>", "<C-f>zz")
+vim.keymap.set("n", "<C-b>", "<C-b>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- =================================================
+-- plugins
+-- =================================================
+local Plug = vim.fn['plug#']
+
+vim.call('plug#begin')
+
+Plug("nvim-treesitter/nvim-treesitter", { ['branch'] = 'master'})
+Plug("numToStr/Comment.nvim")
+Plug("williamboman/mason.nvim")
+Plug("neovim/nvim-lspconfig")
+Plug("williamboman/mason-lspconfig.nvim")
+Plug('blazkowolf/gruber-darker.nvim')
+
+vim.call('plug#end')
+
+require("gruber-darker").setup({
+    bold = true,
+    italic = {
+        strings = false,
+        comments = false,
+        operators = false,
+        folds = false
+    }
+})
+vim.cmd.colorscheme("gruber-darker")
+vim.api.nvim_set_hl(0, '@property', { link = "GruberDarkerFG1" })
+vim.api.nvim_set_hl(0, '@function.call', { link = "GruberDarkerFG1" })
+vim.api.nvim_set_hl(0, '@type.definition', { link = "GruberDarkerFG1" })
+vim.api.nvim_set_hl(0, '@punctuation.bracket', { link = "GruberDarkerFG1" })
+vim.api.nvim_set_hl(0, '@type.builtin', { link = "GruberDarkerQuartz" })
+vim.api.nvim_set_hl(0, '@constant.builtin', { link = "GruberDarkerQuartz" })
+vim.api.nvim_set_hl(0, '@label', { link = "GruberDarkerFG1" })
+
+-- LSP
+require("mason").setup {}
+local servers = {
+    omnisharp = {
+        cmd = { vim.fn.stdpath('data') .. "/mason/packages/omnisharp/OmniSharp", "-z", "--hostPID", "12345", "DotNet:enablePackageRestore=false", "--encoding", "utf-8", "--languageserver" }
+    },
+    pylsp = {},
+    cssls = {},
+    html = {},
+    clangd = {},
+    gopls = {},
+    ts_ls = {},
+    vue_ls = {},
+    lua_ls = {
+        settings = {
+            Lua = {
+                runtime = { version = 'LuaJIT', },
+                diagnostics = { globals = { "vim" }, },
+                workspace = { library = vim.api.nvim_get_runtime_file("", true), },
+                telemetry = { enable = false, },
+            },
+        }
+    }
+}
+
+for server, config in pairs(servers) do
+    vim.lsp.config[server] = config
+    vim.lsp.enable(server)
+end
+
+-- format buffer command
+vim.api.nvim_create_user_command('Format', function(_)
+    vim.lsp.buf.format()
+end, {});
+
+vim.diagnostic.config({
+    virtual_text = true
+})
+
+-- Treesitter
+require('nvim-treesitter.configs').setup({
+    ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
+    modules = {},
+    ignore_install = {},
+    sync_install = false,
+    auto_install = true,
+    highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+    },
+    indent = { enable = true }
+})
+
+vim.highlight.priorities.semantic_tokens = 95
+
